9019b9524897d718791c42baac4f7575f37fcb65
BezaDir <bezadir@gmail.com>
2022-12-08 02:28:51 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-Start point
+ *Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+

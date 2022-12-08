7d696869388ed950b9adb7e38f717ae493b8ba33
BezaDir <bezadir@gmail.com>
2022-12-08 01:58:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *Use the function puts
-*the end value must be 0
+*End value -0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

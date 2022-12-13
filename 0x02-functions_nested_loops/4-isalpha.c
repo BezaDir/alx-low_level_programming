54e4f6d96942aaa04901fd02b8b8a4be911815a3
BezaDir <bezadir@gmail.com>
2022-12-13 03:13:54 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,13 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z') || (c == ' '))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z'))
+	{
+		if ((c == ' ') || (c >= 0 && c <= 9))
+		{
+			return (0);
+		}
 		return (1);
+	}
 	return (0);
 }

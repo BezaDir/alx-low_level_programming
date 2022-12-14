b4104299a28d8181ac47132a9698860e0ba48434
BezaDir <bezadir@gmail.com>
2022-12-13 22:15:03 -0800

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int num, multi, prod;
 
 	for  (num = 0; num <= 9; num++)
 	{
@@ -17,8 +17,10 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0')
-			_putchar((prod % 10) + '0');
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}

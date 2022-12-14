ac817ab0899003c7da1934c4939901dda1164d23
BezaDir <bezadir@gmail.com>
2022-12-13 23:24:43 -0800

    Prints natural number multples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,8 +9,11 @@ int main(void)
 {
 	int i, sum = 0;
 
-	for (i = 3; i < 1024; i += 3)
-		sum += i;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 	printf("%d\n", sum);
 	return (0);
 }

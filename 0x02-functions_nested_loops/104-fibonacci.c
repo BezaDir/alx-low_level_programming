adce5672eb804ef3481244efa5e3a3a413f87111
BezaDir <bezadir@gmail.com>
2022-12-14 01:29:13 -0800

    Prints the first 98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descriptiom : print the first 98 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long int n1 = 0, n2 = 1, fn;
+	int i;
+
+	for (i = 1; i <= 98; i++)
+	{
+		fn = n1 + n2;
+		if (i == 98)
+			printf("%ld\n", fn);
+		else
+			printf("%ld, ", fn);
+		n1 = n2;
+		n2 = fn;
+		fn = n1 + n2;
+	}
+	return (0);
+}

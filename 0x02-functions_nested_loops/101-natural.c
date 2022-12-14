4b1b0fa6d177b14013b91a022b883de3e103ef44
BezaDir <bezadir@gmail.com>
2022-12-13 23:20:18 -0800

    Prints natural number multples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints natural number below 1024
+ * i.e, multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 3; i < 1024; i += 3)
+		sum += i;
+	printf("%d\n", b);
+	return (0);
+}

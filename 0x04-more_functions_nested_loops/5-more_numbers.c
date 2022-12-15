6dbae234cb8c3b4982b57487200ac2cf7c26ac35
BezaDir <bezadir@gmail.com>
2022-12-15 02:26:04 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
 
 void more_numbers(void)
 {
-	int count, num;
+	int i, j;
 
-	for (count = 0; count < 10; count++)
+	for (i = 0; i < 10; i++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (num > 9)
-				_putchar((num / 10) + '0');
-			_putchar((y % 10) + '0');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}

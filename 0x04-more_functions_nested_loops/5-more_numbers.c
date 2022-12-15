68231b86297ebcbe95ebe3ace41855e528b6899c
BezaDir <bezadir@gmail.com>
2022-12-15 02:19:43 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,7 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-				_putchar((num / 10) + '0')
+				_putchar((num / 10) + '0');
 			_putchar((y % 10) + '0');
 		}
 	}

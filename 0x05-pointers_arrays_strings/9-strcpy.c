719a9f8cf783847ba1211d3d87f76d51e3a58bc2
BezaDir <bezadir@gmail.com>
2022-12-20 03:06:36 -0800

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]  != '\0'; i++)
+		dest[i] = src[i];
+	dest[i++] = '\0';
+	return (dest);
+}

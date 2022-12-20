ff607f0fd6b677cb2a1dbb591155b9ff23743c3b
BezaDir <bezadir@gmail.com>
2022-12-20 02:45:42 -0800

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be checked
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

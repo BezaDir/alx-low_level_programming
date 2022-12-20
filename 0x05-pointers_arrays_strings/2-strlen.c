afea16171de60ca017aeda802271c05169c4271b
BezaDir <bezadir@gmail.com>
2022-12-20 01:50:36 -0800

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check
+ * Return: string lengthe
+ */
+
+int _strlen(char *s)
+{
+	int ch = 0;
+
+	for (; *s != '\0'; s++)
+		ch++;
+	return (ch);
+}

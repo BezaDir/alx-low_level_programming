b70e345f40fa352df25c88a79846a821244e2592
BezaDir <bezadir@gmail.com>
2022-12-21 03:56:34 -0800

    Change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @s: string
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}

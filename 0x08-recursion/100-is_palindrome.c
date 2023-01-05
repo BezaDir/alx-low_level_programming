8681c1b62577eb7c13b1c96bd3f654675eaf2cf4
BezaDir <bezadir@gmail.com>
2023-01-05 03:26:02 -0800

    Is plaindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 int check_palindrome(char *s);
 
@@ -10,9 +11,10 @@ int check_palindrome(char *s);
 
 int is_palindrome(char *s)
 {
-	if (*s == '0')
-		return (1);
-	return (check_palindrome(s));
+	int len = _strlen_recursion(s);
+	int count = 0;
+
+	return (check_palindrome(s, len - 1, count));
 }
 /**
  * check_palindrome - Check if a string is palindrome
@@ -21,16 +23,11 @@ int is_palindrome(char *s)
  */
 int check_palindrome(char *s)
 {
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-		return (0);
-	return (1);
+	if (count >= len)
+		return (1);
+	if (str[len] == str[count])
+		return (check_palindrome(str, len - 1, count + 1));
+	return (0);
 }
 /**
  * _strlen_recursion - Get the length of a string
@@ -39,8 +36,10 @@ int check_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (_strlen_recursion(s) + 1);
+	if (*s)
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	return (0);
 }

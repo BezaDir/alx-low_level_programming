6d5eb12e997d80a122881ad244a0a781d0041d67
BezaDir <bezadir@gmail.com>
2023-01-12 03:46:10 -0800

    3-array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	a = malloc(sizeof(*ar) * ((max - min) + 1));
+	a = malloc(sizeof(*a) * ((max - min) + 1));
 
 	if (a == NULL)
 		return (NULL);

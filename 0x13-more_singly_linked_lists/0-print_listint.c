e26baf6f5edc2bb353d3fa556d1ec025bec3e0f2
BezaDir <bezadir@gmail.com>
2023-01-30 21:50:08 -0800

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t cont = 0;
 
-	while (h)
+	for (; h != NULL; cont++)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
-	return (num);
+	return (cont);
 }

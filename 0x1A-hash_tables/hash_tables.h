10332e597f178588db1c5ea03a7aeeabcc68b24f
BezaDir <bezadir@gmail.com>
2023-04-10 11:01:04 +0300

    Hash Table Header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -60,10 +60,8 @@ typedef struct hash_node_s
 /**
  * struct hash_table_s - Hash table data structure
  *
- * @size: The size of the array
+ * @size: Array Size
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
